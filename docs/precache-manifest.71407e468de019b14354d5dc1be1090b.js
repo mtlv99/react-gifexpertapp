0ad74811026c426e2cdfddd61adfed04f3fd579d
Marco Leon <marco.tulio.leon@gmail.com>
2020-09-24 17:49:57 -0600

    Carpeta docs para GitHub Pages agregada

---
+++ docs/precache-manifest.71407e468de019b14354d5dc1be1090b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33165c102dd51627695ca6b4dbeb9a3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2fb82b64a7e87cae504",
+    "url": "/static/css/main.6fad29db.chunk.css"
+  },
+  {
+    "revision": "100caeaeffd01e7cd693",
+    "url": "/static/js/2.78557d1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78557d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2fb82b64a7e87cae504",
+    "url": "/static/js/main.7cd01713.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
